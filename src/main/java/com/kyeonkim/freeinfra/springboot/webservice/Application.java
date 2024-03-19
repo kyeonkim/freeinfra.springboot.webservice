65bd9fefd8bedc86a7bc8b22cf7d799c771482e1
kyeonkim <kyeonkim5021@gmail.com>
2024-03-19 19:57:34 +0900

    init

---
+++ src/main/java/com/kyeonkim/freeinfra/springboot/webservice/Application.java
@@ -0,0 +1,13 @@
+package com.kyeonkim.freeinfra.springboot.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
