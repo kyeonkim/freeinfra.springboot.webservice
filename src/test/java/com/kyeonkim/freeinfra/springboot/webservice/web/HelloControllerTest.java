2011d2ddbd733fc7c275da9afa51c9e228ee6be9
kyeonkim <kyeonkim5021@gmail.com>
2024-03-20 21:04:28 +0900

    Create && Update: jpa ORM, h2 DB 추가 및 junit4 > junit5 로 변경 - DB를 효율적으로 사용하기 위한 jpa 추가 - 테스트를 위해 h2 DB 추가 - 오래전에 junit4 에서 junit5로 변경됨. junit5에 맞춰 수정

--- src/test/java/com/kyeonkim/freeinfra/springboot/webservice/web/HelloControllerTest.java
+++ src/test/java/com/kyeonkim/freeinfra/springboot/webservice/web/HelloControllerTest.java
@@ -1,20 +1,18 @@
 package com.kyeonkim.freeinfra.springboot.webservice.web;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest
 public class HelloControllerTest {
 
