23c6ee33e23eb531e7467832fc24c3045b44856a
kyeonkim <kyeonkim5021@gmail.com>
2024-03-25 17:44:46 +0900

    Feat: JPA Auditing 으로 생성시간/수정시간 자동화하기 - 보통 엔티티에는 해당 데이터의 생성시간과 수정시간을 포함. 언제 만들어졌는지 수정되었는지 등 차후 유지보수에 있어 굉장히 중요함. 매번 날짜 데이터를 등록/수정하는 코드가 여기저기 들어가기에 JPA Auditing 을 이용

--- src/main/java/com/kyeonkim/freeinfra/springboot/webservice/domain/posts/Posts.java
+++ src/main/java/com/kyeonkim/freeinfra/springboot/webservice/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.kyeonkim.freeinfra.springboot.webservice.domain.posts;
 
+import com.kyeonkim.freeinfra.springboot.webservice.domain.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
