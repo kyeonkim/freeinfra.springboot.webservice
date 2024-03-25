d4f54f24931af4302ac878958030943474ac922c
kyeonkim <kyeonkim5021@gmail.com>
2024-03-25 15:42:46 +0900

    Feat: 게시판 수정 및 조회 API 추가 - 수정 테스트 코드 작성 - h2 console 을 이용하여 조회 테스트 진행 > console 로 데이터 insert 후 만들어진 아이디로 /api/v1/posts/{id}로 확인

--- src/main/java/com/kyeonkim/freeinfra/springboot/webservice/service/posts/PostsService.java
+++ src/main/java/com/kyeonkim/freeinfra/springboot/webservice/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.kyeonkim.freeinfra.springboot.webservice.service.posts;
 
+import com.kyeonkim.freeinfra.springboot.webservice.domain.posts.Posts;
 import com.kyeonkim.freeinfra.springboot.webservice.domain.posts.PostsRepository;
+import com.kyeonkim.freeinfra.springboot.webservice.web.dto.PostsResponseDto;
 import com.kyeonkim.freeinfra.springboot.webservice.web.dto.PostsSaveRequestDto;
+import com.kyeonkim.freeinfra.springboot.webservice.web.dto.PostsUpdateRequestDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +18,19 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity())
                 .getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
---
+++ src/main/java/com/kyeonkim/freeinfra/springboot/webservice/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.kyeonkim.freeinfra.springboot.webservice.web.dto;
+
+import com.kyeonkim.freeinfra.springboot.webservice.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
