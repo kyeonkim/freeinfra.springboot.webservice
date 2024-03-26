547dc4fa93319971192fce1284a841f42fdbe248
kyeonkim <kyeonkim5021@gmail.com>
2024-03-26 18:52:14 +0900

    Feat: mustache 를 이용하여 index 페이지 생성 - #4 문제 해결

--- src/test/java/com/kyeonkim/freeinfra/springboot/webservice/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/kyeonkim/freeinfra/springboot/webservice/web/dto/HelloResponseDtoTest.java
@@ -1,6 +1,6 @@
 package com.kyeonkim.freeinfra.springboot.webservice.web.dto;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
