206a91b67e9c2141505b33540a5e3084c93b5ce8
kyeonkim <kyeonkim5021@gmail.com>
2024-04-03 20:37:15 +0900

    Feat: LoginUser Annotation 추가 - 세션값을 HttpSession으로 매번 가져오게되면 사용할 때마다 중복코드가 생김으로 Annotation 으로 만들어 parameter 로 받을 수 있도록 함

--- src/main/java/com/kyeonkim/freeinfra/springboot/webservice/web/IndexController.java
+++ src/main/java/com/kyeonkim/freeinfra/springboot/webservice/web/IndexController.java
@@ -1,9 +1,9 @@
 package com.kyeonkim.freeinfra.springboot.webservice.web;
 
+import com.kyeonkim.freeinfra.springboot.webservice.config.auth.LoginUser;
 import com.kyeonkim.freeinfra.springboot.webservice.config.auth.dto.SessionUser;
 import com.kyeonkim.freeinfra.springboot.webservice.service.posts.PostsService;
 import com.kyeonkim.freeinfra.springboot.webservice.web.dto.PostsResponseDto;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,12 +15,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
